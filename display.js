7b06129e3240b119c7271675022a5b1b218c260b
Philly-P <panzarpa@clarkson.edu>
2024-05-05 14:27:58 -0400

    changes
    testing things

--- display.js
+++ display.js
@@ -1,35 +1,23 @@
 
 function getSpell() {
-    let selector = document.getElementById('select');
-    selector.disabled = false;
-    let selected = document.getElementById('select').value;
-    let spellName = document.getElementById('name');
-    let Action = document.getElementById('castTime');
-    let feet = document.getElementById('range');
-    let des = document.getElementById('description');
-    let sch = data.filter(spell => spell.school === selected);
-    let rand = Math.floor(Math.random() * sch.length);
-    //spellName.innerText = sch[rand].text;
-      if (sch[rand].spellName === null) {
-        spellName.innerText = "Anonymous";
-      } else {
-        spellName.innerText = sch[rand].spellName;
-      };
-      if (sch[rand].Action === null) {
-        Action.innerText = "Anonymous";
-      } else {
-        Action.innerText = sch[rand].Action;
-      };
-      if (sch[rand].feet === null) {
-        feet.innerText = "Anonymous";
-      } else {
-        feet.innerText = sch[rand].feet;
-      };
-      if (sch[rand].des === null) {
-        des.innerText = "Anonymous";
-      } else {
-        des.innerText = sch[rand].des;
-      };
+  let selector = document.getElementById('select');
+  selector.disabled = false;
+  let selected = document.getElementById('select').value;
+  let spellDes = document.getElementById('description');
+  let time = document.getElementById('castTime');
+  let spellname = document.getElementById('name');
+  let far = document.getElementById('range');
+  let spellschool = data.filter(spell => spell.school === selected);
+  let rand = Math.floor(Math.random() * spellschool.length);
+  spellDes.innerText = spellschool[rand].description;
+    if (spellschool[rand].name === null) {
+      spellname.innerText = "Magic Missle 1st level spell";
+    } else {
+      spellname.innerText = spellschool[rand].name;
+      time.innerText = spellschool[rand].castTime;
+      far.innerText = spellschool[rand].range;
+    };
+      
     }
 
     
